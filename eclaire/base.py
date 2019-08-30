454ad5caec07c6c579c10504ef18fd7db0cd88f9
Jake Barber <jake.b.dev@gmail.com>
2019-08-30 10:30:45 +1000

    Update copyright

--- eclaire/base.py
+++ eclaire/base.py
@@ -1,4 +1,4 @@
-# Copyright 2015 KOGAN.COM PTY LTD
+# Copyright 2019 KOGAN.COM PTY LTD
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- eclaire/main.py
+++ eclaire/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2015 KOGAN.COM PTY LTD
+# Copyright 2019 KOGAN.COM PTY LTD
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
